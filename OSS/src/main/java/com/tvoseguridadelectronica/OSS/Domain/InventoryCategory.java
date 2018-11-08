d7fe126468414cb1166b5b02d4bd052355688423
Koralia Rodríguez González <koranrg02@gmail.com>
2018-11-08 14:35:50 -0600

    Categoria domain

---
+++ OSS/src/main/java/com/tvoseguridadelectronica/OSS/Domain/InventoryCategory.java
@@ -0,0 +1,31 @@
+package com.tvoseguridadelectronica.OSS.Domain;
+
+public class InventoryCategory {
+
+    private int id;
+    private String name;
+
+    public InventoryCategory(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public InventoryCategory() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
