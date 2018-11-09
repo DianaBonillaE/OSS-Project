343e2b88d7f4b077e1ee0d527d2458b0f1327d29
Andres Coto <andres.coto3539@gmail.com>
2018-11-08 22:41:23 -0600

    insert and delete

--- OSS/src/main/java/com/tvoseguridadelectronica/OSS/Repository/ModelBrandDao.java
+++ OSS/src/main/java/com/tvoseguridadelectronica/OSS/Repository/ModelBrandDao.java
@@ -1,4 +1,104 @@
 package com.tvoseguridadelectronica.OSS.Repository;
 
+import com.tvoseguridadelectronica.OSS.Domain.ModelBrand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+@Repository
 public class ModelBrandDao {
+
+    private SimpleJdbcCall simpleJdbcCall;
+    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcCall = new SimpleJdbcCall(dataSource);
+    }
+
+    class ModelBrandRowMapper implements RowMapper<ModelBrand> {
+
+        @Override
+        public ModelBrand mapRow(ResultSet rs, int i) throws SQLException {
+
+            ModelBrand modelBrand = new ModelBrand();
+            modelBrand.setId(rs.getInt("id"));
+            modelBrand.setModel(rs.getString("model"));
+            modelBrand.setBrand(rs.getString("brand"));
+
+            return modelBrand;
+        }
+    }
+
+    public List<ModelBrand> findAll(){
+        String consulta ="select * from Model_Brand";
+        return this.jdbcTemplate.query(consulta, new ModelBrandRowMapper());
+
+    }
+
+    public ModelBrand insert(ModelBrand modelBrand)throws SQLException{
+
+        Connection connection = dataSource.getConnection();
+        String sqlInsert = "{call OSS_Model_Brand_Insert (?,?)}";
+
+        CallableStatement statement = connection.prepareCall(sqlInsert);
+        //statement.registerOutParameter(1, Types.INTEGER);
+        statement.setString(1, modelBrand.getModel());
+        statement.setString(2, modelBrand.getBrand());
+
+        statement.execute();
+        statement.close();
+        connection.close();
+
+        return modelBrand;
+    }
+
+  /*  public ProductCategory findById(int productCategoryId){
+
+        String sqlProcedure = "{call Panaris_Product_Category_FindById (?)}";
+
+        ProductCategory productCategory= this.jdbcTemplate.queryForObject(sqlProcedure,new CategoryRowMapper(),productCategoryId);
+
+        return productCategory;
+    }*/
+
+    public void update (ModelBrand modelBrand) {
+
+        simpleJdbcCall.withProcedureName("OSS_Model_Brand_Update");
+        Map<String,Object> inParamMap = new HashMap<String,Object>();
+        inParamMap.put("id", modelBrand.getId());
+        inParamMap.put("model", modelBrand.getModel());
+        inParamMap.put("brand", modelBrand.getBrand());
+        SqlParameterSource in = new MapSqlParameterSource(inParamMap);
+        simpleJdbcCall.execute(in);
+
+    }
+
+    public void delete(int modelBrandId)throws SQLException{
+        System.out.println("entrooooo");
+
+        Connection connection = dataSource.getConnection();
+        String sqlDelete = "{call OSS_Model_Brand_Delete (?)}";
+
+        CallableStatement statement =connection.prepareCall(sqlDelete);
+        statement.setInt(1, modelBrandId);
+
+        statement.execute();
+        statement.close();
+        connection.close();
+    }
+
 }
--- OSS/src/main/java/com/tvoseguridadelectronica/OSS/RestController/ModelBrandRestController.java
+++ OSS/src/main/java/com/tvoseguridadelectronica/OSS/RestController/ModelBrandRestController.java
@@ -1,5 +1,74 @@
 package com.tvoseguridadelectronica.OSS.RestController;
 
-public class ModelBrandRestController {
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.tvoseguridadelectronica.OSS.Domain.ModelBrand;
+import com.tvoseguridadelectronica.OSS.Repository.ModelBrandDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
+import java.util.List;
+//@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@RestController
+@RequestMapping("/api/modelbrand")
+public class ModelBrandRestController {
+    private ModelBrandDao modelBrandDao;
+
+    @Autowired
+    public void setModelBrandDao(ModelBrandDao modelBrandDao) { this.modelBrandDao = modelBrandDao; }
+
+   /* @GetMapping("/")
+    public ResponseEntity<List<ProductCategory>> listAllCategories(){
+        List<ProductCategory> categories = categoryDao.findAll();
+        return new ResponseEntity<List<ProductCategory>>(categories, HttpStatus.OK);
+    }
+
+    @GetMapping("/lista")
+    public ResponseEntity<List<ProductCategory>> listAllCategoriesNotProduct(){
+        List<ProductCategory> categories = categoryDao.findNotProduct();
+        return new ResponseEntity<List<ProductCategory>>(categories,HttpStatus.OK);
+    }
+
+    @GetMapping("/{productCategoryId}")
+    public ResponseEntity<ProductCategory> productCategoryById(@PathVariable("productCategoryId") final Integer id){
+        ProductCategory productCategory = categoryDao.findById(id);
+        return new ResponseEntity<ProductCategory>(productCategory, HttpStatus.OK);
+    }*/
+
+    @PostMapping(value="/insertModelBrand")
+    public ResponseEntity<ModelBrand> createModelBrand(@RequestBody final ModelBrand modelBrand){
+
+        ModelBrand modelBrandCreate= null;
+
+        try {
+            modelBrandDao.insert(modelBrand);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<ModelBrand>(modelBrandCreate,HttpStatus.CREATED);
+    }
+
+    @PutMapping(value="/{id}")
+    public ResponseEntity<ModelBrand> updateModelBrand(@PathVariable ("id") final Integer id, @RequestBody  final ModelBrand modelBrand){
+        System.out.println( id+ "  "+modelBrand.getBrand());
+        ModelBrand modelBrandActual = modelBrandDao.findById(id);
+        modelBrandActual.setModel(modelBrand.getModel());
+        modelBrandActual.setBrand(modelBrand.getBrand());
+        modelBrandDao.update(modelBrandActual);
+
+        return new ResponseEntity<ProductCategory>( productCategory,HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ModelBrand> deleteModelBrand (@PathVariable("id") final Integer id){
+        System.out.println(id);
+        try {
+            modelBrandDao.delete(id);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity<ModelBrand>(HttpStatus.NO_CONTENT);
+    }
 }
