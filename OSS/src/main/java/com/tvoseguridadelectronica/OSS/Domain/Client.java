ff4b25a82bdba2794c853491ff4e803a840baa5f
DanielHdzL <dhernandezleiva@gmail.com>
2018-11-08 18:23:50 -0600

    CRUD Client sin Telephone

--- OSS/src/main/java/com/tvoseguridadelectronica/OSS/Domain/Client.java
+++ OSS/src/main/java/com/tvoseguridadelectronica/OSS/Domain/Client.java
@@ -1,4 +1,44 @@
 package com.tvoseguridadelectronica.OSS.Domain;
 
+import java.util.List;
+
 public class Client {
+
+    private String id;
+    private String name;
+    private String contactName;
+
+
+    public Client() {
+    }
+
+    public Client(String id, String name, String contactName) {
+        this.id = id;
+        this.name = name;
+        this.contactName = contactName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
 }
