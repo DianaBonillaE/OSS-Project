e1d918d43b1865dd3940d17f9ad6469ed4b5aa88
TITO\Tito <tito.badilla@hotmail.com>
2018-11-08 18:19:41 -0600

    Device CRUD Ready

--- OSS/src/main/java/com/tvoseguridadelectronica/OSS/RestController/DeviceRestController.java
+++ OSS/src/main/java/com/tvoseguridadelectronica/OSS/RestController/DeviceRestController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +21,8 @@ import com.tvoseguridadelectronica.OSS.Repository.DeviceDao;
 
 
 
+
+
 @RestController
 @RequestMapping({"/api/device"})
 public class DeviceRestController {
@@ -29,14 +30,44 @@ public class DeviceRestController {
 	@Autowired
 	private DeviceDao deviceDao;
 	
-	
+	@GetMapping
+	public ResponseEntity<List<Device>> getAllDevices() throws SQLException {
+
+		List<Device> list= deviceDao.devicesAll();
+		return new ResponseEntity<List<Device>>(list, HttpStatus.OK);
+
+	}
 	
 	@PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Device> createDevice(@RequestBody final Device device) throws SQLException {
 
-		deviceDao.save(device);
-		
+		deviceDao.saveDevice(device);		
 		return new ResponseEntity<Device>(device, HttpStatus.OK);
+
+	}
+	
+	@PutMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Device> updateDevice(@RequestBody final Device device) throws SQLException {
+
+		deviceDao.updateDevice(device);
+		return new ResponseEntity<Device>(device, HttpStatus.NO_CONTENT);
+
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Device> findDeviceById(@PathVariable("id") final String id ) throws SQLException {
+
+		Device device = deviceDao.findDeviceById(id);
+		return new ResponseEntity<Device>(device, HttpStatus.OK);
+
+	}
+	
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Device> delete(@PathVariable("id") final String id ) throws SQLException {
+
+		deviceDao.deleteDevice(id);
+		return new ResponseEntity<Device>(HttpStatus.NO_CONTENT);
 
 	}
 	
